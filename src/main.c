836c45ea4448b68e238d45a7b3b9208873ed564a
lo67 <astop_1310@mail.ru>
2019-11-21 11:15:33 +0300

    Реализация ДКА

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 #define stateNumber 5
 #define charType 5
 
@@ -13,7 +14,54 @@ int main() {
           {0, 0, 0, 0, 0},
       };
 
+  int action;
+  do {
+    printf("(1) Check the string\n"
+           "(2) Exit\n"
+           "Enter the action:\n");
+    scanf("%d", &action);
+
+    if (action == 1) {
+      printf("Enter the string:\n");
+      char str[50] = {};
+      gets(str);
+
+      int currentState = 1;
+
+      for (int i = 0; i < strlen(str); i++) {
+        char currentSymbol = str[i];
+        int currentColumn = getTransitionTableColumn(currentSymbol, currentState);
+        currentState = stateTransitionArray[currentState][currentColumn];
+      }
+
+      if (currentState == 4) {
+        printf("String is correct\n");
+      } else {
+        printf("String is incorrect\n");
+      }
+    }
+  } while (action != 2);
   return 0;
+}
+
+int getTransitionTableColumn(char symbol, int currentState) {
+  if (currentState == 3 && isEscapedSymbol(symbol)) {
+    return 3;
+  }
+
+  if (symbol == '"') {
+    return 0;
+  }
+
+  if (isCharSymbol(symbol)) {
+    return 1;
+  }
+
+  if (symbol == '\\') {
+    return 2;
+  }
+
+  return 4;
 }
 
 int isCharSymbol(char symbol) {
@@ -22,27 +70,4 @@ int isCharSymbol(char symbol) {
 
 int isEscapedSymbol(char symbol) {
   return symbol == '\\' || symbol == 'n' || symbol == 't' || symbol == '"';
-}
-
-int getTransitionTableColumn(char symbol) {
-  switch (symbol) {
-    case '"':
-      return 0;
-    case '\\':
-      return 2;
-    default:
-      return getColumnOfCompositeSymbol(symbol);
-  }
-}
-
-int getColumnOfCompositeSymbol(char symbol) {
-  if (isCharSymbol(symbol)) {
-    return 1;
-  }
-
-  if (isEscapedSymbol(symbol)) {
-    return 3;
-  }
-
-  return 4;
 }
