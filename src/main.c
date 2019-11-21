8bc1bc88b8968f56ca5657e38e20c965a1d450b6
lo67 <astop_1310@mail.ru>
2019-11-21 09:57:47 +0300

    Реализация фнукции проверки текущего символа на соответствие char

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello, World!\n");
   return 0;
+}
+
+int isCharSymbol(char symbol) {
+  return  symbol != '"' && symbol != '\\';
 }
--- src/main.h
+++ src/main.h
@@ -1,4 +1,6 @@
 #ifndef TOKEN_FINDER__MAIN_H_
 #define TOKEN_FINDER__MAIN_H_
 
+int isCharSymbol(char symbol);
+
 #endif  // TOKEN_FINDER__MAIN_H_
