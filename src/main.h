a8283fb10f8ad17c1efd78c95584c1b43540ee79
casper <133casper@gmail.com>
2019-11-21 10:21:24 +0300

    Функция определения столбца таблицы переходов

--- src/main.h
+++ src/main.h
@@ -3,6 +3,10 @@
 
 int isCharSymbol(char symbol);
 
-int isEscapeSymbol(char symbol);
+int isEscapedSymbol(char symbol);
+
+int getTransitionTableColumn(char symbol);
+
+int getColumnOfCompositeSymbol(char symbol);
 
 #endif  // TOKEN_FINDER__MAIN_H_
