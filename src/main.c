7d1816bf42e91ee72a8201d1846160b49328af78
casper <133casper@gmail.com>
2019-11-21 09:57:32 +0300

    Функция определения экранируемого символа

--- src/main.c
+++ src/main.c
@@ -5,3 +5,7 @@ int main() {
   printf("Hello, World!\n");
   return 0;
 }
+
+int isEscapedSymbol(char symbol) {
+  return symbol == '\\' || symbol == 'n' || symbol == 't' || symbol == '"';
+}
--- src/main.h
+++ src/main.h
@@ -1,4 +1,6 @@
 #ifndef TOKEN_FINDER__MAIN_H_
 #define TOKEN_FINDER__MAIN_H_
 
+int isEscapeSymbol(char symbol);
+
 #endif  // TOKEN_FINDER__MAIN_H_
